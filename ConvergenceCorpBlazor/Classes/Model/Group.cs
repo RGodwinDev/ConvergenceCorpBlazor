abd41731fb8416e8b788441b95cd50793c9b23c1
Ryan Godwin <ryan.godwin524@outlook.com>
2025-12-26 14:41:13 -0600

    dbcontrollers and sqldbc added, cleaning up program.cs and groups.cs

--- ConvergenceCorpBlazor/Classes/Model/Group.cs
+++ ConvergenceCorpBlazor/Classes/Model/Group.cs
@@ -1,155 +1,157 @@
-/// <summary>
-/// A Group object.
-/// </summary>
-public class Group
+namespace ConvergenceCorpBlazor.Classes.Model
 {
     /// <summary>
-    /// List of GroupRuns in the Group
+    /// A Group object.
     /// </summary>
-    private List<GroupRun> Runs = [];
-    
-    public int Id { get; }
-
-    /// <summary>
-    /// Link to the logo.
-    /// </summary>
-    public string? Logo { get; set; }
-
-    /// <summary>
-    /// The Group's name.
-    /// </summary>
-    public string Name { get; set; }
-
-    /// <summary>
-    /// The Group's tag, 0-4 Characters.
-    /// </summary>
-    public string? Guildtag { get; set; }
-
-    /// <summary>
-    /// List of the Groups Links
-    /// a link is a Tuple<string, string>
-    /// first is the actual link
-    /// second is the type (discord, twitch, etc...)
-    /// </summary>
-    private readonly List<Tuple<string, string>> links = new List<Tuple<string, string>>();
-
-    public Group(int id, string logo, string name, string guildtag)
+    public class Group
     {
-        this.Id = id;
-        this.Name = name;
-        this.Guildtag = guildtag;
-        this.Logo = logo;
-        Groups.getList().Add(this);
-        Groups.AddGroupToGroups(this);
-    }
+        /// <summary>
+        /// List of GroupRuns in the Group
+        /// </summary>
+        private List<GroupRun> Runs = [];
 
-    /// <summary>
-    /// Constructor
-    /// </summary>
-    /// <param name="logolink">The link to the logo.</param>
-    /// <param name="name">The Groups Name.</param>
-    /// <param name="guildtag">The Groups Guild Tag (1-4 Characters).</param>
-    /// <param name="linklist">List of Links.</param>
-    public Group(int id, string logo, string name, string guildtag, List<Tuple<string, string>> linklist)
-    {
-        this.Id = id;
-        this.Name = name;
-        this.Guildtag = guildtag;
-        this.Logo = logo;//default logo here
-        this.links = linklist;
-        Groups.getList().Add(this);
-        Groups.AddGroupToGroups(this);
-    }
+        public int Id { get; }
 
-    /// <summary>
-    /// Gets ALL the runs for this Group.
-    /// </summary>
-    /// <returns>List of GroupRuns</returns>
-    public List<GroupRun> GetALLRuns()
-    {
-        return this.Runs;
-    }
+        /// <summary>
+        /// Link to the logo.
+        /// </summary>
+        public string? Logo { get; set; }
 
-    /// <summary>
-    /// Gets all the GroupRuns from a Region for this Group.
-    /// </summary>
-    /// <param name="region">'NA' or 'EU'</param>
-    /// <returns>List of GroupRuns</returns>
-    public List<GroupRun> GetRegionRuns(string region)
-    {
-        return [.. Runs.Where(r => r.GetRegion() == region)];
-    }
+        /// <summary>
+        /// The Group's name.
+        /// </summary>
+        public string Name { get; set; }
 
-    /// <summary>
-    /// Gets the link to the logo of the Group.
-    /// </summary>
-    /// <returns>string, link to the logo.</returns>
-    public string GetLogo()
-    {
-        if (this.Logo != null)
+        /// <summary>
+        /// The Group's tag, 0-4 Characters.
+        /// </summary>
+        public string? Guildtag { get; set; }
+
+        /// <summary>
+        /// List of the Groups Links
+        /// a link is a Tuple<string, string>
+        /// first is the actual link
+        /// second is the type (discord, twitch, etc...)
+        /// </summary>
+        private readonly List<Tuple<string, string>> links = new List<Tuple<string, string>>();
+
+        public Group(int id, string logo, string name, string guildtag)
         {
-            return this.Logo;
+            this.Id = id;
+            this.Name = name;
+            this.Guildtag = guildtag;
+            this.Logo = logo;
+            Groups.getList().Add(this);
+            Groups.AddGroupToGroups(this);
         }
-        return "";
-    }
 
-    /// <summary>
-    /// Get the Name of the Group.
-    /// </summary>
-    /// <returns>String name</returns>
-    public string GetName()
-    {
-        return this.Name;
-    }
-
-    /// <summary>
-    /// Get ALL Groups.
-    /// </summary>
-    /// <returns>List of Groups</returns>
-    public static List<Group> GetGroups() //why is this even here?
-    {
-        return Groups.getList();
-    }
-
-    /// <summary>
-    /// Add a run to the Group
-    /// </summary>
-    /// <param name="r">The GroupRun to be added.</param>
-    public void AddRun(GroupRun r)
-    {
-        Runs.Add(r);
-    }
-
-    /// <summary>
-    /// Add a link to the Group.
-    /// </summary>
-    /// <param name="link">first:type('discord', 'twitch', 'website'), second:link</param>
-    public void AddLink(Tuple<string, string> link)
-    {
-        this.links.Add(link);
-    }
-
-    /// <summary>
-    /// Gets all the links of this Group
-    /// </summary>
-    /// <returns>List of Links</returns>
-    public List<Tuple<string, string>> GetLinks()
-    {
-        return this.links;
-    }
-
-    /// <summary>
-    /// Get the Guild Tag of the Group
-    /// </summary>
-    /// <returns>String</returns>
-    public String GetGuildTag()
-    {
-        if (this.Guildtag != null)
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logolink">The link to the logo.</param>
+        /// <param name="name">The Groups Name.</param>
+        /// <param name="guildtag">The Groups Guild Tag (1-4 Characters).</param>
+        /// <param name="linklist">List of Links.</param>
+        public Group(int id, string logo, string name, string guildtag, List<Tuple<string, string>> linklist)
         {
-            return this.Guildtag;
+            this.Id = id;
+            this.Name = name;
+            this.Guildtag = guildtag;
+            this.Logo = logo;//default logo here
+            this.links = linklist;
+            Groups.getList().Add(this);
+            Groups.AddGroupToGroups(this);
         }
-        return "";
-    }
 
+        /// <summary>
+        /// Gets ALL the runs for this Group.
+        /// </summary>
+        /// <returns>List of GroupRuns</returns>
+        public List<GroupRun> GetALLRuns()
+        {
+            return this.Runs;
+        }
+
+        /// <summary>
+        /// Gets all the GroupRuns from a Region for this Group.
+        /// </summary>
+        /// <param name="region">'NA' or 'EU'</param>
+        /// <returns>List of GroupRuns</returns>
+        public List<GroupRun> GetRegionRuns(string region)
+        {
+            return [.. Runs.Where(r => r.GetRegion() == region)];
+        }
+
+        /// <summary>
+        /// Gets the link to the logo of the Group.
+        /// </summary>
+        /// <returns>string, link to the logo.</returns>
+        public string GetLogo()
+        {
+            if (this.Logo != null)
+            {
+                return this.Logo;
+            }
+            return "";
+        }
+
+        /// <summary>
+        /// Get the Name of the Group.
+        /// </summary>
+        /// <returns>String name</returns>
+        public string GetName()
+        {
+            return this.Name;
+        }
+
+        /// <summary>
+        /// Get ALL Groups.
+        /// </summary>
+        /// <returns>List of Groups</returns>
+        public static List<Group> GetGroups() //why is this even here?
+        {
+            return Groups.getList();
+        }
+
+        /// <summary>
+        /// Add a run to the Group
+        /// </summary>
+        /// <param name="r">The GroupRun to be added.</param>
+        public void AddRun(GroupRun r)
+        {
+            Runs.Add(r);
+        }
+
+        /// <summary>
+        /// Add a link to the Group.
+        /// </summary>
+        /// <param name="link">first:type('discord', 'twitch', 'website'), second:link</param>
+        public void AddLink(Tuple<string, string> link)
+        {
+            this.links.Add(link);
+        }
+
+        /// <summary>
+        /// Gets all the links of this Group
+        /// </summary>
+        /// <returns>List of Links</returns>
+        public List<Tuple<string, string>> GetLinks()
+        {
+            return this.links;
+        }
+
+        /// <summary>
+        /// Get the Guild Tag of the Group
+        /// </summary>
+        /// <returns>String</returns>
+        public String GetGuildTag()
+        {
+            if (this.Guildtag != null)
+            {
+                return this.Guildtag;
+            }
+            return "";
+        }
+
+    }
 }
-        
--- ConvergenceCorpBlazor/Classes/Model/GroupRun.cs
+++ ConvergenceCorpBlazor/Classes/Model/GroupRun.cs
@@ -1,38 +1,41 @@
-/// <summary>
-/// A Run
-/// </summary>
-public class GroupRun
+namespace ConvergenceCorpBlazor.Classes.Model
 {
     /// <summary>
-    /// unique run id.
+    /// A Run
     /// </summary>
-    public int Id { get; }
-    public int GroupID{ get; }
-    public string Region { get; set; }
-    public DateTimeOffset DateTime { get; set; }
-    public int Bosses { get; set; } = 0; //31 is all Soto, 448 is all JW     10111011111, first 5 are soto, next 3 are jw, last is voe
+    public class GroupRun
+    {
+        /// <summary>
+        /// unique run id.
+        /// </summary>
+        public int Id { get; }
+        public int GroupID { get; }
+        public string Region { get; set; }
+        public DateTimeOffset DateTime { get; set; }
+        public int Bosses { get; set; } = 0; //31 is all Soto, 448 is all JW     10111011111, first 5 are soto, next 3 are jw, last is voe
 
-    public GroupRun(int id, int groupID, DateTimeOffset datetime, string region, int bosses)
-    {
-        this.Id = id;
-        this.GroupID = groupID;
-        this.Region = region;
-        this.DateTime = datetime;
-        this.Bosses = bosses;
-        Groups.FindGroupById(groupID).AddRun(this);
-    }
-   
-    public string GetRegion()
-    {
-        return this.Region;
-    }
-    public DateTimeOffset GetTime()
-    {
-        return DateTime;
-    }
+        public GroupRun(int id, int groupID, DateTimeOffset datetime, string region, int bosses)
+        {
+            this.Id = id;
+            this.GroupID = groupID;
+            this.Region = region;
+            this.DateTime = datetime;
+            this.Bosses = bosses;
+            Groups.FindGroupById(groupID).AddRun(this);
+        }
 
-    public int GetBosses()
-    {
-        return Bosses;
+        public string GetRegion()
+        {
+            return this.Region;
+        }
+        public DateTimeOffset GetTime()
+        {
+            return DateTime;
+        }
+
+        public int GetBosses()
+        {
+            return Bosses;
+        }
     }
 }
