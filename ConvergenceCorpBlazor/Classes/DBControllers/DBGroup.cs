9fd07fa7099846dda73aacdd2f9f874a02c10bd2
danlyons-home <dan.lyons.home@gmail.com>
2025-12-27 21:48:52 -0600

    Cleanup/refactoring:
    * Remove unused DBGroups type
    * Use file-scoped namespaces
    * Use using declarations
    * Use record types for data storage types
    * Ensure all ADO.NET types are disposed properly
    * Utilize SqlConnectionStringBuilder.DataSource over indexer for server
       address
    * Eliminate unnecessary long-term storage of group dictionary structure
    * Mark classes with only static members as static types themselves
    * Pull links and runs before groups so Group can expose them as
      immutable collections
    * Define a flags enum to store the bosses bitmask in human-readable form
    * Take advantage of Enum.HasFlag for checking boss flags
    * Utilize ValueTuple over Tuple along with tuple decomposition syntax
    * Build region runs via LINQ expressions
    * Prefer use of properties over get methods

--- ConvergenceCorpBlazor/Classes/DBControllers/DBGroup.cs
+++ ConvergenceCorpBlazor/Classes/DBControllers/DBGroup.cs
@@ -1,61 +1,87 @@
 ﻿using ConvergenceCorpBlazor.Classes.Helper;
-using Microsoft.Data.SqlClient;
 using ConvergenceCorpBlazor.Classes.Model;
+using Microsoft.Data.SqlClient;
 
-namespace ConvergenceCorpBlazor.Classes.DBControllers
+namespace ConvergenceCorpBlazor.Classes.DBControllers;
+
+public static class DBGroup
 {
-    public abstract class DBGroup
+    public static void GetAll()
     {
-        public static void GetAll()
+        try
         {
-            try
+            using var conn = new SqlConnection(SQLDBC.ConnectionString);
+            conn.Open();
+
+            var links = GetLinks(conn);
+            var runs = GetRuns(conn);
+
+            using var command = conn.CreateCommand();
+            command.CommandText = "SELECT * FROM Groups;";
+
+            using var reader = command.ExecuteReader();
+            if(!reader.HasRows)
             {
-                SQLDBC.makeConnection();
-                List<Model.Group> groups = new List<Model.Group>();
-                using (SqlConnection conn = SQLDBC.getConnection())
-                {
-                    SqlCommand command = new(
-                        "SELECT * FROM Groups;",
-                        conn);
-                    conn.Open();
-                    SqlDataReader reader = command.ExecuteReader();
-                    int rowcount = 0;
-                    while (reader.HasRows)
-                    {
-                        rowcount++;
-                        while (reader.Read())
-                        {   //the group is added to GroupList and GroupDict in the Group Constructor.
-                            new Model.Group(reader.GetInt32(0), reader.GetString(1), reader.GetString(2), reader.GetString(3));
-                        }
-                        reader.NextResult();
-                    }
-
-                    if (rowcount == 0) { Console.WriteLine("There were no rows"); }
-                    reader.Close();
-
-                    SqlCommand linkCommand = new(
-                        "SELECT * FROM Links",
-                        conn);
-                    SqlDataReader linkReader = linkCommand.ExecuteReader();
-                    int linkcount = 0;
-                    while (linkReader.HasRows)
-                    {
-                        linkcount++;
-                        while (linkReader.Read())
-                        {
-                            Group g = Groups.FindGroupById(linkReader.GetInt32(0));
-                            //1 is the actual link, 2 is the type (discord, twitch, etc...)
-                            g.AddLink(new Tuple<string, string>(linkReader.GetString(1), linkReader.GetString(2)));
-                        }
-                        linkReader.NextResult();
-                        
-                        
-                    }
-                    linkReader.Close();
-                    SQLDBC.closeConnection();
-                }
+                Console.WriteLine("There were no rows");
+                return;
             }
-            catch (Exception ex) { Console.WriteLine(ex.Message); }
-        } //end GetAll()
-    } //end DBGroup
-} //end namespace
+            while (reader.Read())
+            {
+                var id = reader.GetInt32(0);
+                if (!links.TryGetValue(id, out var groupLinks))
+                    groupLinks = [];
+
+                if (!runs.TryGetValue(id, out var groupRuns))
+                    groupRuns = [];
+
+                Groups.Add(new Group(id, reader.GetString(1), reader.GetString(2), reader.GetString(3), groupLinks, groupRuns));
+            }
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+    }
+
+    private static Dictionary<int, List<(string, string)>> GetLinks(SqlConnection conn)
+    {
+        var links = new Dictionary<int, List<(string, string)>>();
+        using var linkCommand = conn.CreateCommand();
+        linkCommand.CommandText = "SELECT * FROM Links";
+        using var linkReader = linkCommand.ExecuteReader();
+        while (linkReader.Read())
+        {
+            var id = linkReader.GetInt32(0);
+            var link = (linkReader.GetString(1), linkReader.GetString(2));
+            if (!links.TryGetValue(id, out var currentLinks))
+            {
+                links.Add(id, [link]);
+            }
+            else
+            {
+                currentLinks.Add(link);
+            }
+        }
+        return links;
+    }
+
+    public static Dictionary<int, List<GroupRun>> GetRuns(SqlConnection conn)
+    {
+        var runs = new Dictionary<int, List<GroupRun>>();
+        using var runsCommand = conn.CreateCommand();
+        runsCommand.CommandText = "SELECT * FROM Runs";
+        using var reader = runsCommand.ExecuteReader();
+        while (reader.Read())
+        {
+            var groupId = reader.GetInt32(1);
+            var run = new GroupRun(reader.GetInt32(0), groupId, reader.GetDateTimeOffset(2), reader.GetString(3), (Bosses)reader.GetInt32(4));
+            if(!runs.TryGetValue(groupId, out var currentRuns))
+            {
+                runs.Add(groupId, [run]);
+            }
+            else
+            {
+                currentRuns.Add(run);
+            }
+        }
+
+        return runs;
+    }
+}
---
+++ ConvergenceCorpBlazor/Classes/Model/Bosses.cs
@@ -0,0 +1,26 @@
+﻿namespace ConvergenceCorpBlazor.Classes.Model;
+
+[Flags]
+public enum Bosses : int
+{
+    None = 0,
+
+    // SoTo
+    DemonKnight = 0b1,
+    DeathWing   = 0b10,
+    HellSister  = 0b100,
+    Sorrow      = 0b1000,
+    Umbriel     = 0b10000,
+
+    //Placeholder = 0b000000,
+
+    // JW
+    Decima      = 0b1000000,
+    Greer       = 0b10000000,
+    Ura         = 0b100000000,
+
+    //Placeholder = 0b1000000000,
+
+    // VoE
+    VoE         = 0b10000000000,
+}
