ea90c827df0e439a9d590c520de0322f15554328
Ryan Godwin <ryan.godwin524@outlook.com>
2025-12-28 08:46:48 -0600

    fix logo and name being backwards in Group.cs

--- ConvergenceCorpBlazor/Classes/Model/Group.cs
+++ ConvergenceCorpBlazor/Classes/Model/Group.cs
@@ -1,6 +1,6 @@
 namespace ConvergenceCorpBlazor.Classes.Model;
 
-public record Group(int Id, string Name, string? Logo, string? GuildTag, IEnumerable<(string, string)> Links, IEnumerable<GroupRun> Runs)
+public record Group(int Id, string? Logo, string Name, string? GuildTag, IEnumerable<(string, string)> Links, IEnumerable<GroupRun> Runs)
 {
     public List<GroupRun> GetRegionRuns(string region) => [.. Runs.Where(r => r.Region == region)];
 }
