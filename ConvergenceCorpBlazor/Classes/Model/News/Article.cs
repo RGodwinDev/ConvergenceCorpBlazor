4688be622f527cc285bd314cdf94a283fb136403
Ryan Godwin <ryan.godwin524@outlook.com>
2026-06-25 17:43:52 -0500

    new things

--- ConvergenceCorpBlazor/Classes/Model/News/Article.cs
+++ ConvergenceCorpBlazor/Classes/Model/News/Article.cs
@@ -15,6 +15,8 @@
 
         //list of articles
         public static List<Article> Articles = [
+            new Article(4, "State of Ectos June 2026!", "/images/news/stateofectos2026.png", new DateTimeOffset(2026, 6,7,15,00,00, new TimeSpan(-5,0,0)), "SunMatrix",
+                "Evaluating the state of Ectos!", "/News/ectosjune2026", ["Guild Wars 2", "Glob of Ectoplasm", "Ecto"]),
             new Article(3, "Anet Announces Guild Wars 3!", "/images/news/gw3announcement.png", new DateTimeOffset(2026, 6,7,15,00,00, new TimeSpan(-5,0,0)), "SunMatrix",
                 "The next evolution of the MMO genre!","/News/anet-announces-gw3",["Guild Wars 3","Guild Wars Franchise", "Summer Game Fest 2026", "Summer Game Fest"]),
             new Article(2, "Dragon Bash 2026!", "/images/news/dragonbash2026.png", new DateTimeOffset(2026,6,7,11,00,00, new TimeSpan(-5,0,0)), "SunMatrix", 
